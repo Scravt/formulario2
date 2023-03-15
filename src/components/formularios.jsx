5f77fcbb3c12d9ad660d79b8f4aef59f8e7a53c9
Gonzalo <34680110ax@gmail.com>
2023-03-15 19:44:30 -0300

    uso de libreria "Rect Hook Forms" y validaciones

--- src/components/formularios.jsx
+++ src/components/formularios.jsx
@@ -4,38 +4,51 @@ import '../App.css'
 
 const Formularios = () => {
 
-const {register, handleSubmit} = useForm()
+const {register, handleSubmit, formState: {errors}, watch } = useForm()
 
 
+const customSubmit = (data) => {
+    console.log(data)
 
+}
     return (
     <>
         <div>formularios</div>
 
-        <form action="">
+        <form onSubmit={handleSubmit(customSubmit)} className='from-react'>
+
             <div className='form-control'>
-                <label htmlFor=""></label>
-                <input type="text" {...register('name')} />
+                <label>Nick</label>
+                <input type="text" {...register('name', {
+                    required:true,
+                    maxLength:9})} />
+                {errors.name?.type === 'required' && <small className='fail'> complete this field</small> }
+                {errors.name?.type === 'maxLength' && <small className='fail'> only 9 characters</small> }
             </div>
-            <div className='form-control' >
-                <label htmlFor="">Country</label>
-                <input type="text" {...register('country')} />
-            </div>
+
             <div className='form-control' >
                 <label htmlFor="">age</label>
-                <input type="Number" {...register('age')} />
+                <input type="Number" {...register('age',{
+                    required:true,
+                    min: 18, 
+                    max:89 })} />
+                {errors.age?.type === 'required' && <small className='fail'> complete this field</small> }
+                {errors.age?.type === 'min' && <small className='fail'> minimum age 10 years</small> }
+                {errors.age?.type === 'max' && <small className='fail'> maximum age 89 years</small> }
             </div>
+
             <div className='form-control' >
-                <label htmlFor="">Address</label>
-                <input type="text" {...register('address')} />
+                <label htmlFor="">email</label>
+                <input type="text" {...register('email',{
+                    required:true,
+                    pattern:/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i}) } />
+                {errors.email?.type === 'required' && <small className='fail'> complete this field</small> }
+                {errors.email?.type === 'pattern' && <small className='fail'> email not valid</small> }
             </div>
-            <div className='form-control'>
-                <label htmlFor="">Shift</label>
-                <input type="Number" {...register('shift')} />
-            </div>
+
             <button type='Submit'>Send</button>           
         </form>
-            </>
+    </>
     
     )
 }
